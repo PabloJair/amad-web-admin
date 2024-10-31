f5526cddfd76939c2e21b5358e429aa1a969cc53
Pablo Angeles <pablo.angeles@eon.com.mx>
2024-10-31 11:03:44 -0600

    Modules

--- modules/layout/src/lib/components/properties-components/image-properties/image-properties.component.ts
+++ modules/layout/src/lib/components/properties-components/image-properties/image-properties.component.ts
@@ -8,10 +8,11 @@ import {
   FileUploadValidators,
 } from '@iplab/ngx-file-upload';
 import { CommonsStrings, getBase64 } from '@amad-web-admin/modules/core';
-import { DialogService } from '@amad-web-admin/modules/ui-elements';
 import { ComponentEntity } from '../../../entities/component-entity';
 import { defaultComponentEntity } from '../../../entities/compontents-utils';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { UploadService } from '@amad-web-admin/modules/network';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'lib-image-properties',
@@ -45,11 +46,11 @@ export class ImagePropertiesComponent {
     ]
   );
 
-  constructor() {
+  constructor(protected uploadImage: UploadService) {
     this.fileUploadControl.valueChanges.subscribe((files) => {
       if (files.length > 0) {
-        getBase64(files[0], (value) => {
-          this.componentEntity().properties.base64Image = value;
+        this.uploadImage.uploadFile(files[0]).subscribe((value) => {
+          this.componentEntity().properties.base64Image = value.data;
         });
       }
     });
